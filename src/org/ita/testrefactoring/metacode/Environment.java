2bac9413698b4f041c66ed00ecc52d59782c6890
Rafael Monico <rmonico1@gmail.com>
2011-02-13 14:51:00 -0200

    Alterações decorrentes da substituição da classe AbstractType pela interface Type.

--- src/org/ita/testrefactoring/metacode/Environment.java
+++ src/org/ita/testrefactoring/metacode/Environment.java
@@ -1,10 +1,9 @@
 package org.ita.testrefactoring.metacode;
 
-import java.util.List;
 import java.util.Map;
 
 public interface Environment {
 	public abstract Map<String, ? extends Package> getPackageList();
 	
-	public abstract Map<String, ? extends AbstractType> getTypeCache();
+	public abstract Map<String, ? extends Type> getTypeCache();
 }
