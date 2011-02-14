58fdd9ca593cbb6cbbe500afaaacaec770aac6dd
Rafael Monico <rmonico1@gmail.com>
2011-02-14 16:55:07 -0200

    Implementação mínima de ClassVisitor

--- src/org/ita/testrefactoring/ASTParser/ClassParser.java
+++ src/org/ita/testrefactoring/ASTParser/ClassParser.java
@@ -1,17 +1,47 @@
 package org.ita.testrefactoring.ASTParser;
 
-class ClassParser implements ASTTypeParser {
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+
+class ClassParser implements ASTTypeParser<ASTClass> {
 	
-	private ASTType type;
+	private class ClassVisitor extends ASTVisitor {
+
+		private ASTClass clazz;
+
+		public void setClass(ASTClass clazz) {
+			this.clazz = clazz;
+		}
+		
+		@Override
+		public boolean visit(FieldDeclaration node) {
+			// TODO: Continuar daqui
+			ASTField field = clazz.createField(node.toString());
+			
+			return false;
+		}
+		
+		@Override
+		public boolean visit(MethodDeclaration node) {
+			return false;
+		}
+	}
+
+	private ASTClass clazz;
 
 	@Override
-	public void setType(ASTType type) {
-		this.type = type;
+	public void setType(ASTClass type) {
+		this.clazz = type;
 	}
 
 	@Override
 	public void parse() {
-		type.getASTObject();
+		ClassVisitor visitor = new ClassVisitor();
+		
+		visitor.setClass(clazz);
+		
+		clazz.getASTObject().accept(visitor);
 	}
 
 }
