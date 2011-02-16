8634ee6ba2747c7caa413ca1cba3a442cec5e9c3
Rafael Monico <rmonico1@gmail.com>
2011-02-16 11:39:51 -0200

    Tipo general para quando o fonte não estiver disponível para parsing.

---
+++ src/org/ita/testrefactoring/astparser/DummyType.java
@@ -0,0 +1,17 @@
+package org.ita.testrefactoring.astparser;
+
+import org.ita.testrefactoring.metacode.Package;
+
+public class DummyType extends org.ita.testrefactoring.metacode.DummyType {
+
+	@Override
+	protected void setName(String name) {
+		super.setName(name);
+	}
+	
+	@Override
+	protected void setPackage(Package pack) {
+		// TODO Auto-generated method stub
+		super.setPackage(pack);
+	}
+}
