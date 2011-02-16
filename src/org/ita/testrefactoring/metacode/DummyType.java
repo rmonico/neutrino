234e0a17032bb3abee1ca85b2bc15418035f6179
Rafael Monico <rmonico1@gmail.com>
2011-02-16 11:20:40 -0200

    DummyType virou interface

--- src/org/ita/testrefactoring/metacode/DummyType.java
+++ src/org/ita/testrefactoring/metacode/DummyType.java
@@ -1,10 +1,5 @@
 package org.ita.testrefactoring.metacode;
 
-import java.util.Map;
-
-import org.eclipse.jdt.core.dom.TypeDeclaration;
-import org.ita.testrefactoring.astparser.ASTType;
-import org.ita.testrefactoring.astparser.TypeKind;
 
 
 /**
@@ -13,54 +8,6 @@ import org.ita.testrefactoring.astparser.TypeKind;
  * @author Rafael Monico
  * 
  */
-public class DummyType implements Type {
-
-	@Override
-	public SourceFile getSourceFile() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Package getPackage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public TypeAccessModifier getAccessModifier() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Map<String, ? extends Field> getFieldList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Map<String, Method> getMethodList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public TypeKind getKind() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getQualifiedName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public interface DummyType extends Type {
 
 }
