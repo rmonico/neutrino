9b0b568ba2b0b997e3d7453a20c375d2de4932d5
Rafael Monico <rmonico1@gmail.com>
2011-02-16 11:19:30 -0200

    Superclasse alterada para Object e implementado interface Type (implementação não funcional).

--- src/org/ita/testrefactoring/metacode/DummyType.java
+++ src/org/ita/testrefactoring/metacode/DummyType.java
@@ -1,5 +1,7 @@
 package org.ita.testrefactoring.metacode;
 
+import java.util.Map;
+
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.ita.testrefactoring.astparser.ASTType;
 import org.ita.testrefactoring.astparser.TypeKind;
@@ -11,15 +13,54 @@ import org.ita.testrefactoring.astparser.TypeKind;
  * @author Rafael Monico
  * 
  */
-public class DummyType extends ASTType {
+public class DummyType implements Type {
+
+	@Override
+	public SourceFile getSourceFile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Package getPackage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TypeAccessModifier getAccessModifier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, ? extends Field> getFieldList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Method> getMethodList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
 	public TypeKind getKind() {
-		return TypeKind.UNKNOWN;
-	}
-	
-	@Override
-	public TypeDeclaration getASTObject() {
+		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public String getQualifiedName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
