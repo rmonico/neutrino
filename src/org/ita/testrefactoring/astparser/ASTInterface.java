bb90623b2f0aad42e7780325245f28198fd8ef88
Rafael Monico <rmonico1@gmail.com>
2011-02-16 11:13:18 -0200

    Visibilidade do setParent alterada para default

--- src/org/ita/testrefactoring/astparser/ASTInterface.java
+++ src/org/ita/testrefactoring/astparser/ASTInterface.java
@@ -11,7 +11,7 @@ public class ASTInterface extends ASTType implements Interface {
 		return parent;
 	}
 	
-	protected void setParent(Interface _interface) {
+	void setParent(Interface _interface) {
 		this.parent = _interface;
 	}
 
