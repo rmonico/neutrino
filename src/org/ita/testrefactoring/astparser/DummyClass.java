1f49c4e2613dcbc2697671313861007c866413a4
Rafael Monico <rmonico1@gmail.com>
2011-02-17 12:21:39 -0200

    Nova classe, indica que um tipo dummy é uma classe.

---
+++ src/org/ita/testrefactoring/astparser/DummyClass.java
@@ -0,0 +1,16 @@
+package org.ita.testrefactoring.astparser;
+
+import org.ita.testrefactoring.metacode.Package;
+
+public class DummyClass extends org.ita.testrefactoring.metacode.DummyClass {
+
+	@Override
+	protected void setName(String name) {
+		super.setName(name);
+	}
+	
+	@Override
+	protected void setPackage(Package pack) {
+		super.setPackage(pack);
+	}
+}
