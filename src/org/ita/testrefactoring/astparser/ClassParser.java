59d9ddf0c2ec69ab8727e94067e1cc570fddd589
Rafael Monico <rmonico1@gmail.com>
2011-02-17 13:56:31 -0200

    Implementação da promoção de tipo.

--- src/org/ita/testrefactoring/astparser/ClassParser.java
+++ src/org/ita/testrefactoring/astparser/ClassParser.java
@@ -3,12 +3,13 @@ package org.ita.testrefactoring.astparser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.ita.testrefactoring.metacode.AlreadyPromotedTypeException;
 import org.ita.testrefactoring.metacode.Class;
 import org.ita.testrefactoring.metacode.ParserException;
 import org.ita.testrefactoring.metacode.Type;
 
 class ClassParser implements ASTTypeParser<ASTClass> {
-	
+
 	private class ClassVisitor extends ASTVisitor {
 
 		private ASTClass clazz;
@@ -16,30 +17,29 @@ class ClassParser implements ASTTypeParser<ASTClass> {
 		public void setClass(ASTClass clazz) {
 			this.clazz = clazz;
 		}
-		
+
 		@Override
 		public boolean visit(FieldDeclaration node) {
 			// TODO: Continuar daqui
 			ASTField field = clazz.createField(node.toString());
-			
-//			field.setInitialization()
+
+			// field.setInitialization()
 			field.setParentType(clazz);
-			
-			
+
 			String fieldTypeQualifiedName = node.getType().resolveBinding().getQualifiedName();
-			
+
 			ASTEnvironment environment = clazz.getPackage().getEnvironment();
 			Type fieldType = environment.getTypeCache().get(fieldTypeQualifiedName);
-			
+
 			if (fieldType == null) {
 				fieldType = environment.createDummyClass(fieldTypeQualifiedName);
 			}
-			
+
 			field.setFieldType(fieldType);
-			
+
 			return false;
 		}
-		
+
 		@Override
 		public boolean visit(MethodDeclaration node) {
 			return false;
@@ -56,38 +56,46 @@ class ClassParser implements ASTTypeParser<ASTClass> {
 	@Override
 	public void parse() throws ParserException {
 		ClassVisitor visitor = new ClassVisitor();
-		
+
 		visitor.setClass(clazz);
-		
+
 		String superClassName;
-		
+
 		org.eclipse.jdt.core.dom.Type superclassNode = clazz.getASTObject().getSuperclassType();
-		
+
 		if (superclassNode == null) {
 			superClassName = "java.lang.Object";
 		} else {
 			superClassName = superclassNode.resolveBinding().getQualifiedName();
 		}
-		
+
 		ASTEnvironment environment = clazz.getPackage().getEnvironment();
 		Type superClass = environment.getTypeCache().get(superClassName);
-		
+
 		if (superClass == null) {
 			superClass = environment.createDummyClass(superClassName);
 		}
-		
-		// Antes não era possível saber qual o Kind do tipo, agora sabe-se que se trata de uma classe, promovê-lo
+
+		// Antes não era possível saber qual o Kind do tipo, agora sabe-se que
+		// se trata de uma classe, promovê-lo
 		if (superClass.getKind() == TypeKind.UNKNOWN) {
-			superClass = environment.promoteDummyTypeToDummyClass(superClass);
+			DummyClass dummyClass = environment.createDummyClass(superClassName);
+			try {
+				superClass.promote(dummyClass);
+			} catch (AlreadyPromotedTypeException e) {
+				assert false : "Nunca deveria acontecer...";
+				e.printStackTrace();
+			}
+			superClass = dummyClass;
 		}
-		
-		if ((superClass.getKind() != TypeKind.CLASS) && (superClass.getKind() != TypeKind.UNKNOWN)) {
+
+		if (superClass.getKind() != TypeKind.CLASS) {
 			throw new ParserException("Super classe de \"" + clazz.getQualifiedName() + "\" inválida (\"" + superClass.getQualifiedName() + ")");
 		}
-		
+
 		// Aqui superClass deve ser uma classe, já que getKind devolveu CLASS...
 		clazz.setParent((Class) superClass);
-		
+
 		// TODO: Popular os modificadores da classe
 
 		clazz.getASTObject().accept(visitor);
