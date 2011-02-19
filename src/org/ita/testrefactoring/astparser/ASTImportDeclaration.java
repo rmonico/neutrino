8d31a08976c51774a598fb5e77ef34c1021cca1e
Rafael Monico <rmonico1@gmail.com>
2011-02-19 17:49:15 -0200

    type renomeado para importedType e implementação do listener de tipo.

--- src/org/ita/testrefactoring/astparser/ASTImportDeclaration.java
+++ src/org/ita/testrefactoring/astparser/ASTImportDeclaration.java
@@ -2,13 +2,23 @@ package org.ita.testrefactoring.astparser;
 
 import org.ita.testrefactoring.metacode.ImportDeclaration;
 import org.ita.testrefactoring.metacode.Type;
+import org.ita.testrefactoring.metacode.TypeListener;
 
 public class ASTImportDeclaration implements ImportDeclaration, ASTWrapper<org.eclipse.jdt.core.dom.ImportDeclaration> {
 
 	private boolean isStatic;
-	private Type type;
+	private Type importedType;
+	private TypeListener importedTypeListener = new ImportedTypeListener();
 	private ASTSourceFile parent;
 	private org.eclipse.jdt.core.dom.ImportDeclaration astObject;
+
+	private class ImportedTypeListener implements TypeListener {
+
+		@Override
+		public void typePromoted(Type oldType, Type newType) {
+			importedType = newType;
+		}
+	}
 
 	// Construtor acessível somente através do pacote
 	ASTImportDeclaration() {
@@ -16,12 +26,20 @@ public class ASTImportDeclaration implements ImportDeclaration, ASTWrapper<org.e
 	}
 
 	protected void setType(Type type) {
-		this.type = type;
+		if (this.importedType != null) {
+			this.importedType.removeListener(importedTypeListener);
+		}
+
+		this.importedType = type;
+
+		if (this.importedType != null) {
+			this.importedType.addListener(importedTypeListener);
+		}
 	}
-	
+
 	@Override
 	public Type getType() {
-		return type;
+		return importedType;
 	}
 
 	protected void setSourceFile(ASTSourceFile parent) {
@@ -36,7 +54,7 @@ public class ASTImportDeclaration implements ImportDeclaration, ASTWrapper<org.e
 	protected void setStatic(boolean isStatic) {
 		this.isStatic = isStatic;
 	}
-	
+
 	@Override
 	public boolean isStatic() {
 		return isStatic;
@@ -49,23 +67,23 @@ public class ASTImportDeclaration implements ImportDeclaration, ASTWrapper<org.e
 	public org.eclipse.jdt.core.dom.ImportDeclaration getASTObject() {
 		return astObject;
 	}
-	
+
 	public void setASTObject(org.eclipse.jdt.core.dom.ImportDeclaration astObject) {
 		this.astObject = astObject;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		
+
 		sb.append("import ");
-		
+
 		if (isStatic) {
 			sb.append("static ");
 		}
-			
-		sb.append(type.getQualifiedName() + ";");
-		
+
+		sb.append(importedType.getQualifiedName() + ";");
+
 		return sb.toString();
 	}
 
