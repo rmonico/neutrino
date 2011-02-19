6533e875aa1c1cbbf095ffed08fe6158ee75001a
Rafael Monico <rmonico1@gmail.com>
2011-02-19 21:32:48 -0200

    Implementado ouvinte para o parent.

--- src/org/ita/testrefactoring/astparser/ASTInterface.java
+++ src/org/ita/testrefactoring/astparser/ASTInterface.java
@@ -1,18 +1,39 @@
 package org.ita.testrefactoring.astparser;
 
 import org.ita.testrefactoring.metacode.Interface;
+import org.ita.testrefactoring.metacode.Type;
+import org.ita.testrefactoring.metacode.TypeListener;
 
 public class ASTInterface extends ASTType implements Interface {
 
+	private ParentTypeListener parentTypeListener = new ParentTypeListener();
 	private Interface parent;
 
+	private class ParentTypeListener implements TypeListener {
+
+		@Override
+		public void typePromoted(Type oldType, Type newType) {
+			parent = (Interface) newType;
+		}
+		
+	}
+	
+	
 	@Override
 	public Interface getParent() {
 		return parent;
 	}
 	
 	void setParent(Interface _interface) {
+		if (parent != null) {
+			parent.removeListener(parentTypeListener);
+		}
+		
 		this.parent = _interface;
+		
+		if (parent != null) {
+			parent.addListener(parentTypeListener);
+		}
 	}
 
 	@Override
