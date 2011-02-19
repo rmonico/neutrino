857b11d879baece50c81f30673d6ef3497f5d27a
Rafael Monico <rmonico1@gmail.com>
2011-02-19 21:40:03 -0200

    Implementação de ouvinte para campo superClass

--- src/org/ita/testrefactoring/metacode/DummyClass.java
+++ src/org/ita/testrefactoring/metacode/DummyClass.java
@@ -1,23 +1,41 @@
 package org.ita.testrefactoring.metacode;
 
-
 /**
  * Representa um tipo onde tudo o que sei é que se trata de uma classe.
+ * 
  * @author Rafael Monico
- *
+ * 
  */
 public class DummyClass extends DummyType implements Class {
 
 	private NonAccessClassModifier nonAccessClassModifier;
+	private SuperClassListener parentListener = new SuperClassListener();
 	private Class superClass;
-	
+
+	private class SuperClassListener implements TypeListener {
+
+		@Override
+		public void typePromoted(Type oldType, Type newType) {
+			superClass = (Class) newType;
+		}
+
+	}
+
 	@Override
 	public Class getSuperClass() {
 		return superClass;
 	}
-	
+
 	protected void setSuperClass(Class superClass) {
+		if (this.superClass != null) {
+			this.superClass.removeListener(parentListener);
+		}
+
 		this.superClass = superClass;
+
+		if (this.superClass != null) {
+			this.superClass.addListener(parentListener);
+		}
 	}
 
 	@Override
