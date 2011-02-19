df6a8e5dc3486195467aeac7cc77fa77b17e9e45
Rafael Monico <rmonico1@gmail.com>
2011-02-19 21:26:08 -0200

    Nova classe, responsável por atualizar o listener de tipo dos itens do Map ouvido.

---
+++ src/org/ita/testrefactoring/astparser/WrappedMapListener.java
@@ -0,0 +1,41 @@
+package org.ita.testrefactoring.astparser;
+
+import org.ita.testrefactoring.metacode.Type;
+import org.ita.testrefactoring.metacode.TypeListener;
+import org.zero.utils.IMapListener;
+
+/**
+ * Implementação de IMapListener<String, ? extends Type> responsável por
+ * atualizar o listener de tipo dos itens do Map ouvido.
+ * 
+ * @author Rafael Monico
+ * 
+ * @param <T>
+ */
+public class WrappedMapListener<T extends Type> implements IMapListener<String, T> {
+
+	private TypeListener listener;
+
+	public void setTypeListener(TypeListener listener) {
+		this.listener = listener;
+	}
+
+	@Override
+	public void put(String key, T newValue, T oldValue) {
+		if (oldValue != null) {
+			oldValue.removeListener(listener);
+		}
+
+		if (newValue != null) {
+			newValue.addListener(listener);
+		}
+	}
+
+	@Override
+	public void remove(String key, T removedValue) {
+		if (removedValue != null) {
+			removedValue.removeListener(listener);
+		}
+	}
+
+}
