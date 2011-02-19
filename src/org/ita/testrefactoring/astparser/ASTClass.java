aad7f5d6d259dd89c7b43e1ea4f36100222d9197
Rafael Monico <rmonico1@gmail.com>
2011-02-19 21:37:24 -0200

    Implementação de ouvinte para campo parent

--- src/org/ita/testrefactoring/astparser/ASTClass.java
+++ src/org/ita/testrefactoring/astparser/ASTClass.java
@@ -2,12 +2,25 @@ package org.ita.testrefactoring.astparser;
 
 import org.ita.testrefactoring.metacode.Class;
 import org.ita.testrefactoring.metacode.NonAccessClassModifier;
+import org.ita.testrefactoring.metacode.Type;
+import org.ita.testrefactoring.metacode.TypeListener;
 
 public class ASTClass extends ASTType implements Class {
 	
 	// Vai dar problema depois, pois NonAccessClassModifier é read-only
 	private NonAccessClassModifier nonAccessModifier = new NonAccessClassModifier();
+	private ParentListener parentListener = new ParentListener();
 	private Class parent;
+	
+	
+	private class ParentListener implements TypeListener {
+
+		@Override
+		public void typePromoted(Type oldType, Type newType) {
+			parent = (Class) newType;
+		}
+		
+	}
 
 	@Override
 	public NonAccessClassModifier getNonAccessModifier() {
@@ -20,7 +33,15 @@ public class ASTClass extends ASTType implements Class {
 	}
 	
 	protected void setParent(Class parent) {
+		if (this.parent != null) {
+			this.parent.removeListener(parentListener);
+		}
+		
 		this.parent = parent;
+		
+		if (this.parent != null) {
+			this.parent.addListener(parentListener);
+		}
 	}
 
 	@Override
