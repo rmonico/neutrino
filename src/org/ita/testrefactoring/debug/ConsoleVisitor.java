674efa954d0fb59f6c339e8e26d4a586227a759b
Rafael Monico <rmonico1@gmail.com>
2011-02-20 15:24:34 -0300

    Nova classe de de debug: mostra todos os elementos de um ASTNode no console.

---
+++ src/org/ita/testrefactoring/debug/ConsoleVisitor.java
@@ -0,0 +1,32 @@
+package org.ita.testrefactoring.debug;
+
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.ita.testrefactoring.astparser.QuickVisitor;
+
+
+/**
+ * Classe criada para finalidade de debug, mostra os nós imeadiatamente inferiores ao nó passado como parâmetro.
+ * @author Rafael Monico
+ *
+ */
+public class ConsoleVisitor  {
+	public void showNodes(ASTNode visitedNode) {
+		List<ASTNode> nodes = new QuickVisitor().quickVisit(visitedNode);
+		
+		System.out.println("--- " + visitedNode.toString() + " ---");
+		System.out.println("Class: " + visitedNode.getClass());
+		System.out.println("Subnodes:");
+
+		for (ASTNode node : nodes) {
+			System.out.println("Class: " + node.getClass());
+			System.out.println("--- " + node.toString() + " ---");
+			System.out.println();
+		}
+		
+		System.out.println("--- " + visitedNode.toString() + " ---");
+		System.out.println();
+	}
+
+}
