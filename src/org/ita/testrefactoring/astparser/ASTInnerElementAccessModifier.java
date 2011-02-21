53ec7add64e4d3357d3923b2e980ce25a328e26a
Rafael Monico <rmonico1@gmail.com>
2011-02-20 21:17:51 -0300

    Modificador de acesso específico para a implementação no parser do AST

---
+++ src/org/ita/testrefactoring/astparser/ASTInnerElementAccessModifier.java
@@ -0,0 +1,7 @@
+package org.ita.testrefactoring.astparser;
+
+import org.ita.testrefactoring.metacode.InnerElementAccessModifier;
+
+public class ASTInnerElementAccessModifier extends InnerElementAccessModifier {
+
+}
