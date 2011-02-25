7b819a7107242f4dbd161f02d0937edc2c256ecf
Rafael Monico <rmonico1@gmail.com>
2011-02-25 16:32:41 -0300

    Criados métodos para o devolver o método que contém o bloco.

--- src/org/ita/testrefactoring/metacode/Block.java
+++ src/org/ita/testrefactoring/metacode/Block.java
@@ -3,6 +3,14 @@ package org.ita.testrefactoring.metacode;
 import java.util.List;
 
 public interface Block {
-	
+
 	public List<? extends Statement> getStatementList();
+
+	/**
+	 * Deve devolver o método onde o bloco está (mesmo que o bloco não esteja
+	 * dentro de outro statement que esteja dentro do método.
+	 * 
+	 * @return
+	 */
+	ConcreteMethod getMethod();
 }
