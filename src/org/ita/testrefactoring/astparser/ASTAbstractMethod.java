d3dc8fbb7311923b0f3807d2e6d8fe542b8c769f
Rafael Monico <rmonico1@gmail.com>
2011-02-25 16:33:28 -0300

    Nada

--- src/org/ita/testrefactoring/astparser/ASTAbstractMethod.java
+++ src/org/ita/testrefactoring/astparser/ASTAbstractMethod.java
@@ -3,5 +3,5 @@ package org.ita.testrefactoring.astparser;
 import org.ita.testrefactoring.metacode.AbstractMethod;
 
 public class ASTAbstractMethod extends ASTMethod implements AbstractMethod {
-
+	
 }
