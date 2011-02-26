8c44ed8cab03c9232ef5aa4eddcc6090fa8dd762
Rafael Monico <rmonico1@gmail.com>
2011-02-26 13:46:23 -0300

    O código ficou quebrado por alterações em LiteralExpression (metacode)

--- src/org/ita/testrefactoring/addexplanation/AddExplanationRefactoring.java
+++ src/org/ita/testrefactoring/addexplanation/AddExplanationRefactoring.java
@@ -4,8 +4,6 @@ import org.ita.testrefactoring.AbstractRefactoring;
 import org.ita.testrefactoring.InitialConditionNotMet;
 import org.ita.testrefactoring.RefactoringException;
 import org.ita.testrefactoring.junitparser.JUnitAssertion;
-import org.ita.testrefactoring.metacode.LiteralExpression;
-import org.ita.testrefactoring.parser.MethodParameter;
 
 public class AddExplanationRefactoring extends AbstractRefactoring {
 
@@ -28,14 +26,14 @@ public class AddExplanationRefactoring extends AbstractRefactoring {
 	}
 
 //	private String explanationString;
-	private JUnitAssertion targetAssertion;
+//	private JUnitAssertion targetAssertion;
 	
 	@Override
 	public InitialConditionNotMet checkInitialConditions() {
 		if (!(getTargetFragment() instanceof JUnitAssertion)) {
 			return new TargetIsNotJUnitAssertion();
 		} else {
-			targetAssertion = (JUnitAssertion) getTargetFragment();
+//			targetAssertion = (JUnitAssertion) getTargetFragment();
 		}
 		
 		JUnitAssertion assertion = (JUnitAssertion) getTargetFragment();
@@ -56,16 +54,16 @@ public class AddExplanationRefactoring extends AbstractRefactoring {
 
 	@Override
 	protected void doRefactor() throws RefactoringException {
-		MethodParameter explanationParameter = new MethodParameter();
-		
-		LiteralExpression explanationStringExpression = new LiteralExpression();
-		
+//		MethodParameter explanationParameter = new MethodParameter();
+//		
+//		LiteralExpression explanationStringExpression = new LiteralExpression();
+//		
 //		explanationStringExpression.setValue(explanationString);
-		
-		explanationParameter.setType("java.lang.String");
-		explanationParameter.setExpression(explanationStringExpression);
-		
-		targetAssertion.getParameters().add(0, explanationParameter);
+//		
+//		explanationParameter.setType("java.lang.String");
+//		explanationParameter.setExpression(explanationStringExpression);
+//		
+//		targetAssertion.getParameters().add(0, explanationParameter);
 	}
 
 	public void setExplanationString(String explanationString) {
