6d7eccb9ee53880b11271c15870e8ed6daa38488
Rafael Monico <rmonico1@gmail.com>
2011-02-26 15:10:31 -0300

    Classe ASTMethodInvocationExpression movida para do pacote metacode para astparser

--- src/org/ita/testrefactoring/astparser/ASTMethodInvocationExpression.java
+++ src/org/ita/testrefactoring/astparser/ASTMethodInvocationExpression.java
@@ -1,15 +1,20 @@
-package org.ita.testrefactoring.metacode;
+package org.ita.testrefactoring.astparser;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.ita.testrefactoring.astparser.AbstractASTExpression;
+import org.ita.testrefactoring.metacode.Expression;
+import org.ita.testrefactoring.metacode.Method;
+import org.ita.testrefactoring.metacode.MethodInvocationExpression;
 
 public class ASTMethodInvocationExpression extends AbstractASTExpression implements MethodInvocationExpression {
 
 	private Method calledMethod;
 	private List<Expression> parameterList = new ArrayList<Expression>();
 
+	public ASTMethodInvocationExpression() {
+	}
+	
 	@Override
 	public Method getCalledMethod() {
 		return calledMethod;
@@ -24,4 +29,8 @@ public class ASTMethodInvocationExpression extends AbstractASTExpression impleme
 		return parameterList;
 	}
 
+	@Override
+	public String toString() {
+		return getCalledMethod().getName();
+	}
 }
