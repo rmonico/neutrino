7a7fac91d19afdc8d0dacfe561c69e10c39c79a8
Rafael Monico <rmonico1@gmail.com>
2011-02-25 21:49:37 -0300

    Criação de tipo dummy movida para o TypeCache (é feita automaticamente)

--- src/org/ita/testrefactoring/astparser/ClassParser.java
+++ src/org/ita/testrefactoring/astparser/ClassParser.java
@@ -11,7 +11,6 @@ import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.ita.testrefactoring.metacode.Class;
 import org.ita.testrefactoring.metacode.ParserException;
 import org.ita.testrefactoring.metacode.Type;
-import org.zero.utils.StringUtils;
 
 class ClassParser implements ASTTypeParser<ASTClass> {
 
@@ -22,29 +21,29 @@ class ClassParser implements ASTTypeParser<ASTClass> {
 		public void setClass(ASTClass clazz) {
 			this.clazz = clazz;
 		}
-		
+
 		@SuppressWarnings("unchecked")
 		@Override
 		public boolean visit(FieldDeclaration fieldDeclaration) {
 			List<ASTNode> nodes = new QuickVisitor().quickVisit(fieldDeclaration);
-			
+
 			Utils.rearrangeArray(nodes, VariableDeclarationFragment.class, Modifier.class);
-			
+
 			VariableDeclarationFragment variableDeclaration = (VariableDeclarationFragment) nodes.get(0);
 
 			// TODO
-//			field.getAccessModifier()
-//			field.getNonAccessModifier()
+			// field.getAccessModifier()
+			// field.getNonAccessModifier()
 
 			ASTField field = clazz.createField(variableDeclaration.getName().toString());
-			
+
 			field.setASTObject(fieldDeclaration);
 
 			// TODO: field.setInitialization
 			field.setParentType(clazz);
 
 			String fieldTypeQualifiedName = fieldDeclaration.getType().resolveBinding().getQualifiedName();
-			
+
 			if (fieldDeclaration.getType().resolveBinding().isPrimitive()) {
 				fieldTypeQualifiedName = "." + fieldTypeQualifiedName;
 			}
@@ -53,80 +52,80 @@ class ClassParser implements ASTTypeParser<ASTClass> {
 			Type fieldType = environment.getTypeCache().get(fieldTypeQualifiedName);
 
 			if (fieldType == null) {
-				String packageName = StringUtils.extractPackageName(fieldTypeQualifiedName);
-				
+				String packageName = ASTEnvironment.extractPackageName(fieldTypeQualifiedName);
+
 				ASTPackage pack = environment.getPackageList().get(packageName);
-				
+
 				if (pack == null) {
 					pack = environment.createPackage(packageName);
 				}
-				
-				String typeName = StringUtils.extractTypeName(fieldTypeQualifiedName);
-					
+
+				String typeName = ASTEnvironment.extractTypeName(fieldTypeQualifiedName);
+
 				fieldType = environment.createDummyType(typeName, pack);
 			}
 
 			field.setFieldType(fieldType);
-			
+
 			return false;
 		}
 
 		@Override
 		public boolean visit(MethodDeclaration methodDeclaration) {
 			String methodName = methodDeclaration.getName().toString();
-			
+
 			ASTInnerElementAccessModifier accessModifier = new ASTInnerElementAccessModifier();
 			ASTMethodDeclarationNonAccessModifier nonAccessModifier = new ASTMethodDeclarationNonAccessModifier();
-			
+
 			for (Object m : methodDeclaration.modifiers()) {
 				// Se é alguma coisa que não foi prevista, ignora
 				if (!(m instanceof Modifier)) {
 					continue;
 				}
 				Modifier modifier = (Modifier) m;
-				
+
 				if (modifier.isAbstract()) {
 					nonAccessModifier.setAbstract(true);
 				}
-				
+
 				if (modifier.isFinal()) {
 					nonAccessModifier.setFinal(true);
 				}
-				
+
 				if (modifier.isStatic()) {
 					nonAccessModifier.setStatic(true);
 				}
-				
+
 				if (modifier.isPublic()) {
 					accessModifier.setPublic();
 				}
-				
+
 				if (modifier.isProtected()) {
 					accessModifier.setProtected();
 				}
-				
+
 				if (modifier.isPrivate()) {
 					accessModifier.setPrivate();
 				}
 			}
-			
+
 			ASTMethod method = clazz.createMethod(methodName, nonAccessModifier.isAbstract());
-			
+
 			method.setASTObject(methodDeclaration);
-			
+
 			method.setAccessModifier(accessModifier);
-			
+
 			method.setNonAccessModifier(nonAccessModifier);
 
 			if (!method.getNonAccessModifier().isAbstract()) {
 				ASTConcreteMethod concreteMethod = (ASTConcreteMethod) method;
 
 				ASTBlock block = concreteMethod.getBody();
-				
+
 				block.setASTObject(methodDeclaration.getBody());
 
 				BlockParser parser = new BlockParser();
-				
+
 				parser.setBlock(block);
 
 				try {
@@ -135,7 +134,7 @@ class ClassParser implements ASTTypeParser<ASTClass> {
 					throw new Error(e);
 				}
 			}
-			
+
 			return false;
 		}
 	}
@@ -153,28 +152,24 @@ class ClassParser implements ASTTypeParser<ASTClass> {
 
 		visitor.setClass(clazz);
 
-		String superClassName;
+		String superClassName = null;
 
 		org.eclipse.jdt.core.dom.Type superclassNode = clazz.getASTObject().getSuperclassType();
-		
-		// TODO: Popular a lista de interfaces implementadas
-		//clazz.getASTObject().superInterfaceTypes()
 
-		if (superclassNode == null) {
-			superClassName = "java.lang.Object";
-		} else {
+		// TODO: Popular a lista de interfaces implementadas
+		// clazz.getASTObject().superInterfaceTypes()
+
+		if (superclassNode != null) {
 			superClassName = superclassNode.resolveBinding().getQualifiedName();
 		}
 
 		ASTEnvironment environment = clazz.getPackage().getEnvironment();
 		Type superClass = environment.getTypeCache().get(superClassName);
 
-		if (superClass == null) {
-			superClass = environment.createDummyClass(superClassName);
-		} else if (superClass.getKind() == TypeKind.UNKNOWN) {
+		if (superClass.getKind() == TypeKind.UNKNOWN) {
 			// Se antes não era possível saber qual o Kind do tipo, agora sei
 			// que se trata de uma classe
-			DummyClass dummyClass = environment.createDummyClass(superClassName);
+			DummyClass dummyClass = environment.createDummyClass(superClass.getName());
 
 			superClass.promote(dummyClass);
 
