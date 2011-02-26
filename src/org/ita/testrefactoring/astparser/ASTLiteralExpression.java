e67e5e46c84ceff2066ce66b4b5c28bdfa8abfbb
Rafael Monico <rmonico1@gmail.com>
2011-02-26 13:55:45 -0300

    Novas classes para representar expressões literais no parser

---
+++ src/org/ita/testrefactoring/astparser/ASTLiteralExpression.java
@@ -0,0 +1,24 @@
+package org.ita.testrefactoring.astparser;
+
+import org.ita.testrefactoring.metacode.LiteralExpression;
+
+
+public class ASTLiteralExpression extends AbstractASTExpression implements LiteralExpression {
+
+	private String value;
+
+	@Override
+	public String getValue() {
+		return value;
+	}
+	
+	protected void setValue(String value) {
+		this.value = value;
+	}
+	
+	@Override
+	public String toString() {
+		return value;
+	}
+	
+}
---
+++ src/org/ita/testrefactoring/astparser/AbstractASTExpression.java
@@ -0,0 +1,7 @@
+package org.ita.testrefactoring.astparser;
+
+import org.ita.testrefactoring.metacode.Expression;
+
+public class AbstractASTExpression implements Expression {
+
+}
