9eb6e03477a4b425acdd1c2fa09e4fc7f5466dfb
Rafael Monico <rmonico1@gmail.com>
2011-02-27 14:03:43 -0300

    Visibilidade do construtor alterada para o pacote

--- src/org/ita/testrefactoring/astparser/ASTLiteralExpression.java
+++ src/org/ita/testrefactoring/astparser/ASTLiteralExpression.java
@@ -2,26 +2,25 @@ package org.ita.testrefactoring.astparser;
 
 import org.ita.testrefactoring.metacode.LiteralExpression;
 
-
 public class ASTLiteralExpression extends ASTAbstractExpression implements LiteralExpression {
 
 	private String value;
 
-	public ASTLiteralExpression() {
+	ASTLiteralExpression() {
 	}
-	
+
 	@Override
 	public String getValue() {
 		return value;
 	}
-	
+
 	protected void setValue(String value) {
 		this.value = value;
 	}
-	
+
 	@Override
 	public String toString() {
 		return value;
 	}
-	
+
 }
