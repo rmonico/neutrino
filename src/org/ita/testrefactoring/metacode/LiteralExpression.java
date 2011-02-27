6216ce29beb02ef68ccceb4ee57ad6f3bae64568
Rafael Monico <rmonico1@gmail.com>
2011-02-27 14:06:18 -0300

    Criada constante para valor null literal

--- src/org/ita/testrefactoring/metacode/LiteralExpression.java
+++ src/org/ita/testrefactoring/metacode/LiteralExpression.java
@@ -2,7 +2,19 @@ package org.ita.testrefactoring.metacode;
 
 
 public interface LiteralExpression extends Expression {
-	
+
+	public static final LiteralExpression NULL_EXPRESSION = new LiteralExpression() {
+		@Override
+		public String getValue() {
+			return "null";
+		};
+
+		@Override
+		public String toString() {
+			return getValue();
+		};
+	};
+
 	public String getValue();
 
 }
