ac128e4da8b2ee65d5bd0178399d9d8be958b12e
Rafael Monico <rmonico1@gmail.com>
2011-02-27 14:21:50 -0300

    Refatorada forma como a expressão null é criada

--- src/org/ita/testrefactoring/metacode/LiteralExpression.java
+++ src/org/ita/testrefactoring/metacode/LiteralExpression.java
@@ -3,18 +3,23 @@ package org.ita.testrefactoring.metacode;
 
 public interface LiteralExpression extends Expression {
 
-	public static final LiteralExpression NULL_EXPRESSION = new LiteralExpression() {
-		@Override
-		public String getValue() {
-			return "null";
-		};
-
-		@Override
-		public String toString() {
-			return getValue();
-		};
-	};
+	public static final LiteralExpression NULL_EXPRESSION = new NullLiteralExpression();
 
 	public String getValue();
 
 }
+
+class NullLiteralExpression implements LiteralExpression {
+	
+	@Override
+	public String getValue() {
+		return "null";
+	};
+
+	@Override
+	public String toString() {
+		return getValue();
+	};
+	
+}
+
