b2cf5514804a4bb0cb9d5e2d7d4d1f06c5af5037
Rafael Monico <rmonico1@gmail.com>
2011-02-28 16:20:42 -0300

    Implementando o wrapper para org.eclipse.jdt.core.dom.Expression

--- src/org/ita/testrefactoring/astparser/ASTAbstractExpression.java
+++ src/org/ita/testrefactoring/astparser/ASTAbstractExpression.java
@@ -2,6 +2,23 @@ package org.ita.testrefactoring.astparser;
 
 import org.ita.testrefactoring.metacode.Expression;
 
-public class ASTAbstractExpression implements Expression {
+public abstract class ASTAbstractExpression implements Expression, ASTWrapper<org.eclipse.jdt.core.dom.Expression> {
+
+	private org.eclipse.jdt.core.dom.Expression astObject;
+	
+	@Override
+	public void setASTObject(org.eclipse.jdt.core.dom.Expression astObject) {
+		this.astObject = astObject;
+	}
+
+	@Override
+	public org.eclipse.jdt.core.dom.Expression getASTObject() {
+		return astObject;
+	}
+
+	@Override
+	public String toString() {
+		return getASTObject().toString();
+	}
 
 }
