d36b9892677f168fee5c948f91d5bcc8effe0c0e
Rafael Monico <rmonico1@gmail.com>
2011-03-06 17:50:06 -0300

    Criado teste completo do parser de testes

--- testfiles/org/ita/testrefactoring/testfiles/junitparsertests/MockClass.java
+++ testfiles/org/ita/testrefactoring/testfiles/junitparsertests/MockClass.java
@@ -2,19 +2,42 @@ package org.ita.testrefactoring.testfiles.junitparsertests;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MockClass {
 
+	private Object fixture0 = new Object();
+	private Object fixture1 = new Object();
+	
+	@Before
+	public void setup() {
+		action();
+	}
+	
 	@Test
-	public void testNothing() {
+	public void testNothing0() {
 		action();
 		
 		assertTrue("Comment", true);
 	}
 
+
+	@Test
+	public void testNothing1() {
+		action();
+		
+		assertTrue("Comment", true);
+	}
+
+	
 	private void action() {
-		// TODO Auto-generated method stub
+		
+	}
+	
+	@After
+	public void teardown() {
 		
 	}
 }
