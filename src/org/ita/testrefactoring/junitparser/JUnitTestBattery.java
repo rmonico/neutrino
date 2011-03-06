d0a79e1678eada7c9334e99fefb699a7b46cf868
Rafael Monico <rmonico1@gmail.com>
2011-03-06 14:54:07 -0300

    Remoção de código antigo de JUnitTestBattery

--- src/org/ita/testrefactoring/junitparser/JUnitTestBattery.java
+++ src/org/ita/testrefactoring/junitparser/JUnitTestBattery.java
@@ -3,65 +3,14 @@ package org.ita.testrefactoring.junitparser;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.core.dom.ASTRequestor;
-import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.ita.testrefactoring.abstracttestparser.TestBattery;
-import org.ita.testrefactoring.abstracttestparser.TestElement;
 import org.ita.testrefactoring.abstracttestparser.TestSuite;
 
 public class JUnitTestBattery extends TestBattery {
 
 	private List<TestSuite> testSuiteList = new ArrayList<TestSuite>();
-	public TestElement selectedFragment;
 
-	private class LocateTestSuitesVisitor extends ASTVisitor {
-
-		private TestElement selectedFragment;
-
-		public boolean visit(TypeDeclaration node) {
-			JUnitTestSuite suite = createTestSuite();
-
-			suite.setTypeDeclaration(node);
-
-			suite.parse();
-
-			if (suite.getTestMethodList().size() == 0) {
-				// Não é uma suite de testes, pois não possui métodos de teste,
-				// descarto
-				suite = null;
-			} else {
-				getTestSuiteList().add(suite);
-
-				if (suite.getSelectedFragment() != null) {
-					selectedFragment = suite.getSelectedFragment();
-				}
-			}
-			// Visito os filhos pois posso encontrar métodos de teste em classes
-			// internas
-			return true;
-		}
-
-		public TestElement getSelectedFragment() {
-			return selectedFragment;
-		}
-	}
-
-	// Criação permitida apenas pelo JUnitParser
 	JUnitTestBattery() {
-	}
-
-	@Override
-	public JUnitTestSuite createTestSuite() {
-		JUnitTestSuite suite = new JUnitTestSuite();
-		suite.setParent(this);
-
-		return suite;
 	}
 
 	@Override
@@ -69,39 +18,4 @@ public class JUnitTestBattery extends TestBattery {
 		return testSuiteList;
 	}
 
-	void parse(ASTParser parser, List<ICompilationUnit> compilationUnits,
-			final ICompilationUnit activeCompilationUnit) {
-		final IProgressMonitor monitor = new NullProgressMonitor();
-
-		monitor.beginTask("Parsing compilation units.", compilationUnits.size());
-
-		// Faz o parsing
-		parser.createASTs(compilationUnits.toArray(new ICompilationUnit[0]),
-				new String[0], new ASTRequestor() {
-
-					private int i;
-
-					@Override
-					public void acceptAST(ICompilationUnit source,
-							CompilationUnit ast) {
-						LocateTestSuitesVisitor visitor = new LocateTestSuitesVisitor();
-
-						ast.accept(visitor);
-
-						if (selectedFragment == null) {
-							selectedFragment = visitor.getSelectedFragment();
-						}
-
-						monitor.worked(++i);
-
-						super.acceptAST(source, ast);
-					}
-				}, monitor);
-
-		monitor.done();
-	}
-
-	TestElement getSelectedFragment() {
-		return selectedFragment;
-	}
 }
