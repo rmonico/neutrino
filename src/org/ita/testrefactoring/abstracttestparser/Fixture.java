a29b22e90d1b9563700f41cb77ec22b78a7b23b1
Rafael Monico <rmonico1@gmail.com>
2011-03-06 17:47:56 -0300

    Nova classe: JUnitFixture, Fixture passou a ser interface

--- src/org/ita/testrefactoring/abstracttestparser/Fixture.java
+++ src/org/ita/testrefactoring/abstracttestparser/Fixture.java
@@ -1,4 +1,4 @@
 package org.ita.testrefactoring.abstracttestparser;
 
-public abstract class Fixture implements TestElement {
+public interface Fixture extends TestElement {
 }
---
+++ src/org/ita/testrefactoring/junitparser/JUnitFixture.java
@@ -0,0 +1,14 @@
+package org.ita.testrefactoring.junitparser;
+
+import org.ita.testrefactoring.abstracttestparser.Fixture;
+import org.ita.testrefactoring.abstracttestparser.TestElement;
+
+public class JUnitFixture implements Fixture {
+
+	@Override
+	public TestElement getParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
