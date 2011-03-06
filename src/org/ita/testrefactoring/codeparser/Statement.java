da8fb9b68c0c0056c04e375e2333bbde96192aa8
Rafael Monico <rmonico1@gmail.com>
2011-03-06 00:27:04 -0300

    Alteração no JavaDoc

--- src/org/ita/testrefactoring/codeparser/Statement.java
+++ src/org/ita/testrefactoring/codeparser/Statement.java
@@ -1,11 +1,11 @@
 package org.ita.testrefactoring.codeparser;
 
 /**
- * Indica que a classe é um elemento de código interno a um bloco.
+ * Classe que representa uma linha de código.
+ * 
  * @author Rafael Monico
  *
  */
 public interface Statement extends CodeElement {
 	
-	Block getParent();
 }
