dd4e99735009e7c76d1794fc9bd5cb268dd8d872
Rafael Monico <rmonico1@gmail.com>
2011-03-07 15:22:11 -0300

    Implementação de Fixture para JUnit

--- src/org/ita/testrefactoring/junitparser/JUnitFixture.java
+++ src/org/ita/testrefactoring/junitparser/JUnitFixture.java
@@ -5,15 +5,28 @@ import org.ita.testrefactoring.abstracttestparser.TestElement;
 
 public class JUnitFixture implements Fixture {
 
+	private TestElement parent;
+	private String name;
+
+	JUnitFixture() {
+	}
+	
 	@Override
 	public TestElement getParent() {
-		// TODO Auto-generated method stub
-		return null;
+		return parent;
+	}
+	
+	void setParent(TestElement parent) {
+		this.parent = parent;
 	}
 
 	@Override
 	public String getName() {
-		return null;
+		return name;
+	}
+	
+	void setName(String name) {
+		this.name = name;
 	}
 
 }
