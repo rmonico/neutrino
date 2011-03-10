b59ea28b90cc7dcf6109f787200a598a5319222b
Rafael Monico <rmonico1@gmail.com>
2011-03-10 11:50:33 -0300

    Implementando a interface TestStatement

--- src/org/ita/testrefactoring/junitparser/JUnitTestStatement.java
+++ src/org/ita/testrefactoring/junitparser/JUnitTestStatement.java
@@ -1,10 +1,12 @@
 package org.ita.testrefactoring.junitparser;
 
 import org.ita.testrefactoring.abstracttestparser.TestStatement;
+import org.ita.testrefactoring.codeparser.Statement;
 
 public abstract class JUnitTestStatement implements TestStatement {
 
 	private JUnitTestMethod parent;
+	private Statement element;
 	
 	void setParent(JUnitTestMethod parent) {
 		this.parent = parent;
@@ -13,5 +15,14 @@ public abstract class JUnitTestStatement implements TestStatement {
 	@Override
 	public JUnitTestMethod getParent() {
 		return parent;
+	}
+	
+	@Override
+	public Statement getCodeElement() {
+		return element;
+	}
+	
+	void setElement(Statement element) {
+		this.element = element;
 	}
 }
