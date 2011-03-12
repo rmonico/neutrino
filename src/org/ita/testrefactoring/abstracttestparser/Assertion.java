76b6d7d27b72dcb24dc9fd39994ee0125f1491b6
Rafael Monico <rmonico1@gmail.com>
2011-03-11 22:25:19 -0300

    Criação do método getExplanationIndex

--- src/org/ita/testrefactoring/abstracttestparser/Assertion.java
+++ src/org/ita/testrefactoring/abstracttestparser/Assertion.java
@@ -6,4 +6,13 @@ public interface Assertion extends TestStatement {
 
 	@Override
 	public MethodInvocationStatement getCodeElement();
+
+	/**
+	 * Devolve o índice do parâmetro para ser usado com a função
+	 * getCodeElement().getParameterList(), caso seja -1 a asserção não possui
+	 * explicação.
+	 * 
+	 * @return
+	 */
+	public int getExplanationIndex();
 }
