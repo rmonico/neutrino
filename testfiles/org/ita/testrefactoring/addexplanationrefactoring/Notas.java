9140b70cbc2b0856966edd430abe45856ee19ad5
Rafael Monico <rmonico1@gmail.com>
2011-03-12 19:39:13 -0300

    Novos arquivos para testar a refatoração adicionar explicação a asserção

---
+++ testfiles/org/ita/testrefactoring/addexplanationrefactoring/Notas.java
@@ -0,0 +1,12 @@
+package org.ita.testrefactoring.addexplanationrefactoring;
+
+public class Notas {
+	public void add(String aluno, double nota) {
+		
+	}
+	
+	public double avg() {
+		return 8.0;
+	}
+
+}
---
+++ testfiles/org/ita/testrefactoring/addexplanationrefactoring/TestNotas.java
@@ -0,0 +1,20 @@
+package org.ita.testrefactoring.addexplanationrefactoring;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestNotas {
+	
+	@SuppressWarnings("unused")
+	@Test
+	private void testNotasTurma() {
+		Notas not = new Notas();
+
+		not.add("João", 8.0);
+		not.add("Pedro", 7.0);
+		not.add("Maria", 9.0);
+
+		assertEquals(not.avg(), 8.0, 0);
+	}
+}
