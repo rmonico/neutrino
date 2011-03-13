32df0f2dd7ec54c622b2a78cf1797bc97cdad4bb
Rafael Monico <rmonico1@gmail.com>
2011-03-12 21:38:46 -0300

    Tabs trocados para espaços

--- testfiles/org/ita/testrefactoring/addexplanationrefactoring/TestNotas.java
+++ testfiles/org/ita/testrefactoring/addexplanationrefactoring/TestNotas.java
@@ -5,16 +5,16 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestNotas {
-	
-	@SuppressWarnings("unused")
-	@Test
-	private void testNotasTurma() {
-		Notas not = new Notas();
+    
+    @SuppressWarnings("unused")
+    @Test
+    private void testNotasTurma() {
+        Notas not = new Notas();
 
-		not.add("João", 8.0);
-		not.add("Pedro", 7.0);
-		not.add("Maria", 9.0);
+        not.add("João", 8.0);
+        not.add("Pedro", 7.0);
+        not.add("Maria", 9.0);
 
-		assertEquals(not.avg(), 8.0, 0);
-	}
+        assertEquals(not.avg(), 8.0, 0);
+    }
 }
