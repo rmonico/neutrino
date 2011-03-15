3a5774ea77848eac8c812080b9773b503215f6ee
Rafael Monico <rmonico1@gmail.com>
2011-03-15 02:30:58 -0300

    Criação do método parameterListItemAdded

--- src/org/ita/testrefactoring/astparser/MethodInvocationHandler.java
+++ src/org/ita/testrefactoring/astparser/MethodInvocationHandler.java
@@ -6,17 +6,37 @@ import java.util.List;
 import org.ita.testrefactoring.codeparser.Expression;
 import org.ita.testrefactoring.codeparser.Method;
 import org.ita.testrefactoring.codeparser.MethodInvocation;
+import org.zero.utils.AbstractListListener;
+import org.zero.utils.ListWrapper;
 
 class MethodInvocationHandler implements MethodInvocation {
 
 	private Method calledMethod;
-	private List<Expression> parameterList = new ArrayList<Expression>();
+	private List<Expression> parameterList;
+	
+	public MethodInvocationHandler() {
+		ListWrapper<Expression> wrapper = new ListWrapper<Expression>(new ArrayList<Expression>());
+		
+		wrapper.addListener(new AbstractListListener<Expression>() {
+			@Override
+			public void add(int index, Expression element) {
+				parameterListItemAdded(index, element);
+			}
+
+		});
+		
+		parameterList = wrapper;
+	}
 	
 	@Override
 	public Method getCalledMethod() {
 		return calledMethod;
 	}
 
+	private void parameterListItemAdded(int index, Expression element) {
+		
+	}
+	
 	@Override
 	public List<Expression> getParameterList() {
 		return parameterList;
