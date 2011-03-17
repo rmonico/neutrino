4eac1c505db514d367498e8ca7fad281ee1f9038
Rafael Monico <rmonico1@gmail.com>
2011-03-17 18:47:53 -0300

    Adicionado javadoc.

--- src/org/ita/testrefactoring/codeparser/Package.java
+++ src/org/ita/testrefactoring/codeparser/Package.java
@@ -2,11 +2,23 @@ package org.ita.testrefactoring.codeparser;
 
 import java.util.Map;
 
+/**
+ * Representa um pacote.
+ * 
+ * @author Rafael Monico
+ * 
+ */
 public interface Package extends CodeElement {
-	
+
+	/**
+	 * Devolve o nome do pacote. Por exemplo, para esse pacote, devolveria
+	 * "org.ita.testrefactoring.codeparser".
+	 * 
+	 * @return
+	 */
 	String getName();
-	
+
 	Map<String, ? extends SourceFile> getSourceFileList();
-	
+
 	Environment getParent();
 }
