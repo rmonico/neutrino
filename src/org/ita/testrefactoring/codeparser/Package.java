fe0147fb9cd91d1515f77b2480deacc43a8432f3
Rafael Monico <rmonico1@gmail.com>
2011-03-17 20:02:12 -0300

    Adicionado javadoc.

--- src/org/ita/testrefactoring/codeparser/Package.java
+++ src/org/ita/testrefactoring/codeparser/Package.java
@@ -18,7 +18,16 @@ public interface Package extends CodeElement {
 	 */
 	String getName();
 
+	/**
+	 * Devolve a lista de arquivos de código fonte. A chave para o map é o nome
+	 * do arquivo, por exemplo, "Package.java"
+	 * 
+	 * @return
+	 */
 	Map<String, ? extends SourceFile> getSourceFileList();
 
+	/**
+	 * Devolve o parent para o Package.
+	 */
 	Environment getParent();
 }
