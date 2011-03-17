7d9f0fee5c22441a2c3eb48c54b453b335b125b3
Rafael Monico <rmonico1@gmail.com>
2011-03-17 05:41:55 -0300

    Novos construtores

--- src/org/ita/testrefactoring/abstracrefactoring/RefactoringException.java
+++ src/org/ita/testrefactoring/abstracrefactoring/RefactoringException.java
@@ -1,9 +1,30 @@
 package org.ita.testrefactoring.abstracrefactoring;
 
+import java.util.List;
+
 public class RefactoringException extends Exception {
+
+	public RefactoringException(List<String> problems) {
+		super(getMessageForProblemList(problems));
+	}
 
 	public RefactoringException(Exception e) {
 		super(e);
+	}
+
+	public static String getMessageForProblemList(List<String> problems) {
+		if (problems.size() == 1) {
+			return problems.get(0);
+		} else {
+			StringBuilder message = new StringBuilder();
+			
+			message.append("The following problems were found: \n\n");
+			for (String problem : problems) {
+				message.append("- " + problem + "\n");
+			}
+			
+			return message.toString();
+		}
 	}
 
 	/**
