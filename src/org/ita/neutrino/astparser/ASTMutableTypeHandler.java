382b8ab1a5cabb971ced4105c15b8313819ee881
Rafael Monico <rmonico1@gmail.com>
2011-07-06 16:43:59 -0300

    Segunda implementação do método createNewMethod

--- src/org/ita/neutrino/astparser/ASTMutableTypeHandler.java
+++ src/org/ita/neutrino/astparser/ASTMutableTypeHandler.java
@@ -2,19 +2,24 @@ package org.ita.neutrino.astparser;
 
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.NormalAnnotation;
 import org.eclipse.jdt.core.dom.PrimitiveType;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
+import org.ita.neutrino.astparser.ASTSourceFile.ASTContainer;
 import org.ita.neutrino.codeparser.Method;
 
 public class ASTMutableTypeHandler extends ASTTypeHandler {
-	
+
 	private ASTType handled;
-	
+
 	public ASTMutableTypeHandler(ASTType handled) {
 		super(handled);
-		
+
 		this.handled = handled;
 	}
-	
+
 	/**
 	 * Deve criar um método usando os objetos do AST para que seja refletido no
 	 * código.
@@ -25,21 +30,39 @@ public class ASTMutableTypeHandler extends ASTTypeHandler {
 	 */
 	public Method createNewMethod(String newMethodName) {
 		// TODO: Continuar aqui, pegar o código do projeto antigo e colocar aqui
-		
-		AST ast = handled.getASTObject().getAST();
-		
+
+		ASTContainer compilationUnitASTContainer = handled.getParent().getASTObject();
+
+		AST ast = compilationUnitASTContainer.getCompilationUnit().getAST();
+
 		MethodDeclaration newSetup = ast.newMethodDeclaration();
 
 		newSetup.setName(ast.newSimpleName("setup"));
-		
+
 		newSetup.setReturnType2(ast.newPrimitiveType(PrimitiveType.VOID));
-		
+
 		String methodSignature = newSetup.getName().toString();
 
 		Method newSetupMethod = createMethod(methodSignature);
+
+		ASTRewrite rewrite = compilationUnitASTContainer.getRewrite();
+
+		// ListRewrite lrw = rewrite.getListRewrite(RefactoringUtils
+		// .getPublicClassFrom(compilationUnitASTContainer.getCompilationUnit()),
+		// TypeDeclaration.BODY_DECLARATIONS_PROPERTY);
+		ListRewrite lrw = rewrite.getListRewrite(handled.getASTObject(), TypeDeclaration.BODY_DECLARATIONS_PROPERTY);
+
+		lrw.insertFirst(newSetup, null);
+
+		NormalAnnotation beforeAnnotation = ast.newNormalAnnotation();
+
+		String[] name = { "org", "junit", "Before" };
+
+		beforeAnnotation.setTypeName(ast.newName(name));
+
+		rewrite.getListRewrite(newSetup, MethodDeclaration.MODIFIERS2_PROPERTY).insertFirst(beforeAnnotation, null);
 		
 		return newSetupMethod;
 	}
-
 
 }
