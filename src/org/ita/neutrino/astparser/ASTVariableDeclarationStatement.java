e6c4f07f0506240cd2f57bb597631e50b54e0404
unknown <BRD@.(none)>
2011-09-19 18:07:42 -0300

    mensagem

--- src/org/ita/neutrino/astparser/ASTVariableDeclarationStatement.java
+++ src/org/ita/neutrino/astparser/ASTVariableDeclarationStatement.java
@@ -1,9 +1,19 @@
 package org.ita.neutrino.astparser;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.Assignment;
+import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
+import org.eclipse.jdt.core.dom.InstanceofExpression;
+import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 import org.ita.neutrino.codeparser.Expression;
@@ -52,16 +62,54 @@ public class ASTVariableDeclarationStatement extends ASTAbstractStatement<ASTNod
 		ASTNode node = getASTObject();
 		ConsoleVisitor.showNodes(node);
 		
+		Block blkParent = (Block) node.getParent();
 		AST objAst = node.getAST();
 		fragment = getFragment(node);
 		ASTNode newNode = fragment.copySubtree(objAst, fragment);
-		
+			
+		//org.eclipse.jdt.core.dom.VariableDeclarationStatement statement = objAst.newVariableDeclarationStatement((VariableDeclarationFragment) newNode);
 		org.eclipse.jdt.core.dom.VariableDeclarationStatement statement = objAst.newVariableDeclarationStatement((VariableDeclarationFragment) newNode);
-
-		setASTObject(statement);
+			
+		/*
+		CompilationUnit cu = (CompilationUnit) node.getParent().getParent().getParent().getParent().getParent();
+		ImportDeclaration id =  objAst.newImportDeclaration();
+		id.setName(objAst.newName(getSimpleNames("ita.bruno.teste.Guitarra")));
+		id.setOnDemand(false);
+		cu.imports().add(id);
+	
+		id =  objAst.newImportDeclaration();
+		id.setName(objAst.newName(getSimpleNames("ita.bruno.teste.Guitarra.eTipoGuitarra")));
+		id.setOnDemand(false);
+		cu.imports().add(id);
+	*/
+		
+		Assignment a = objAst.newAssignment();
+		a.setOperator(Assignment.Operator.ASSIGN);
+		//blkParent.statements().add(objAst.newExpressionStatement(a)); 
+				
+		FieldAccess fa = blkParent.getAST().newFieldAccess();
+		fa.setExpression(objAst.newThisExpression());
+		fa.setName(objAst.newSimpleName("g1"));
+		a.setLeftHandSide(fa);					
+		a.setRightHandSide(objAst.newSimpleName(fragment.getInitializer().toString()));
+		
+		setASTObject(a);
 		// setASTObject(fragment);
 
 	}
+	
+
+	static private String[] getSimpleNames(String qualifiedName) {
+		StringTokenizer st = new StringTokenizer(qualifiedName,".");
+		ArrayList list = new ArrayList();
+		while (st.hasMoreTokens()) {
+			String name = st.nextToken().trim();
+			if (!name.equals("*"))
+				list.add(name);
+		}
+		return (String[]) list.toArray(new String[list.size()]);
+	}
+	
 
 	private VariableDeclarationFragment getFragment(ASTNode node) {
 		QuickVisitor visitor = new QuickVisitor();
