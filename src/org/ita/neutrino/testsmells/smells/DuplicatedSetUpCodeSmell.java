f531bb61aba00b79b14c7e99840f5c34ff30c9f1
rsalmeidafl <rsalmeidafl@gmail.com>
2011-10-12 01:54:52 -0300

    Adicionados bad smells pra detectar código de inicialização e/ou finalização duplicado

---
+++ src/org/ita/neutrino/testsmells/smells/DuplicatedSetUpCodeSmell.java
@@ -0,0 +1,78 @@
+package org.ita.neutrino.testsmells.smells;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.viewers.ISelection;
+import org.ita.neutrino.abstracttestparser.AbstractTestParser;
+import org.ita.neutrino.abstracttestparser.TestStatement;
+import org.ita.neutrino.abstracttestparser.TestSuite;
+import org.ita.neutrino.eclipseaction.ActionException;
+import org.ita.neutrino.extractinitializationmethod.ExtractInitializationMethodAction;
+import org.ita.neutrino.extractmethod.CommonStatementFinder;
+import org.ita.neutrino.testsmells.core.EclipseQuickFix;
+import org.ita.neutrino.testsmells.core.MarkerManager;
+
+import com.google.inject.Inject;
+
+public class DuplicatedSetUpCodeSmell extends TestClassLevelCodeSmell {
+	
+	private final CommonStatementFinder duplicatedCodeFinder;
+	private final DuplicatedSetUpCodeQuickFix quickFix;
+	
+	@Inject
+	public DuplicatedSetUpCodeSmell(CommonStatementFinder duplicatedCodeFinder,
+			DuplicatedSetUpCodeQuickFix quickFix) {
+		this.duplicatedCodeFinder = duplicatedCodeFinder;
+		this.quickFix = quickFix;
+	}
+	
+	@Override
+	public void checkForPresence(TestSuite testClass, MarkerManager markerManager) throws CoreException {
+		List<TestStatement> duplicatedCode =
+			duplicatedCodeFinder.listCommonStatements(testClass, /* fromBegin */ true);
+		
+		if (!duplicatedCode.isEmpty()) {
+			markerManager.addMarker(testClass.getCodeElement(), 
+					"Duplicated set up code", DuplicatedSetUpCodeSmell.class);
+		}
+	}
+	
+	@Override
+	public EclipseQuickFix[] getQuickFixes() {
+		return new EclipseQuickFix[] { quickFix };
+	}
+	
+	public static class DuplicatedSetUpCodeQuickFix implements EclipseQuickFix {
+		
+		private final AbstractTestParser testParser;
+		
+		@Inject
+		public DuplicatedSetUpCodeQuickFix(AbstractTestParser testParser) {
+			this.testParser = testParser;
+		}
+		
+		@Override
+		public String title() {
+			return "Create set up method";
+		}
+
+		@Override
+		public void run(ISelection selection) throws ActionException {	
+			ExtractInitializationMethodAction action = new ExtractInitializationMethodAction() {
+
+				@Override
+				protected AbstractTestParser instantiateParser() {
+					return testParser;
+				}
+			};			
+			action.setSelection(selection);
+			action.run();
+		}
+
+		@Override
+		public String description() {
+			return "Extracts the duplicated setup code into a setup method";
+		}
+	}
+}
---
+++ src/org/ita/neutrino/testsmells/smells/DuplicatedTearDownCodeSmell.java
@@ -0,0 +1,79 @@
+package org.ita.neutrino.testsmells.smells;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.viewers.ISelection;
+import org.ita.neutrino.abstracttestparser.AbstractTestParser;
+import org.ita.neutrino.abstracttestparser.TestStatement;
+import org.ita.neutrino.abstracttestparser.TestSuite;
+import org.ita.neutrino.eclipseaction.ActionException;
+import org.ita.neutrino.extractfinalizationmethod.ExtractFinalizationMethodAction;
+import org.ita.neutrino.extractinitializationmethod.ExtractInitializationMethodAction;
+import org.ita.neutrino.extractmethod.CommonStatementFinder;
+import org.ita.neutrino.testsmells.core.EclipseQuickFix;
+import org.ita.neutrino.testsmells.core.MarkerManager;
+
+import com.google.inject.Inject;
+
+public class DuplicatedTearDownCodeSmell extends TestClassLevelCodeSmell {
+
+	private final CommonStatementFinder duplicatedCodeFinder;
+	private final DuplicatedTearDownCodeQuickFix quickFix;
+	
+	@Inject
+	public DuplicatedTearDownCodeSmell(CommonStatementFinder duplicatedCodeFinder,
+			DuplicatedTearDownCodeQuickFix quickFix) {
+		this.duplicatedCodeFinder = duplicatedCodeFinder;
+		this.quickFix = quickFix;
+	}
+	
+	@Override
+	public void checkForPresence(TestSuite testClass, MarkerManager markerManager) throws CoreException {
+		List<TestStatement> duplicatedCode =
+			duplicatedCodeFinder.listCommonStatements(testClass, /* fromBegin */ false);
+		
+		if (!duplicatedCode.isEmpty()) {
+			markerManager.addMarker(testClass.getCodeElement(), 
+					"Duplicated tear down code", DuplicatedSetUpCodeSmell.class);
+		}
+	}
+	
+	@Override
+	public EclipseQuickFix[] getQuickFixes() {
+		return new EclipseQuickFix[] { quickFix };
+	}
+	
+	public static class DuplicatedTearDownCodeQuickFix implements EclipseQuickFix {
+		
+		private final AbstractTestParser testParser;
+		
+		@Inject
+		public DuplicatedTearDownCodeQuickFix(AbstractTestParser testParser) {
+			this.testParser = testParser;
+		}
+		
+		@Override
+		public String title() {
+			return "Create teardown method";
+		}
+
+		@Override
+		public void run(ISelection selection) throws ActionException {	
+			ExtractFinalizationMethodAction action = new ExtractFinalizationMethodAction() {
+
+				@Override
+				protected AbstractTestParser instantiateParser() {
+					return testParser;
+				}
+			};			
+			action.setSelection(selection);
+			action.run();
+		}
+
+		@Override
+		public String description() {
+			return "Extracts the duplicated teardown code into a teardown method";
+		}
+	}
+}
