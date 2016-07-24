6c9eedb1e025eb7d935851b24ac6d6a43f9e7548
Luiz Wagner <nascimentolwtn@gmail.com>
2016-07-24 05:57:18 -0300

    Preparação para Java8 e JUnit5

---
+++ src/org/ita/neutrino/tparsers/junit5parser/JUnitFixture.java
@@ -0,0 +1,9 @@
+package org.ita.neutrino.tparsers.junit5parser;
+
+public class JUnitFixture extends org.ita.neutrino.tparsers.junitgenericparser.JUnitFixture {
+
+	JUnitFixture() {
+		super();
+	}
+
+}
