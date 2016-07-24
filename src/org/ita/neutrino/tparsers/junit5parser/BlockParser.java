822faafe336705c0e4e25191aa33a9eb03ea6ab2
Luiz Wagner <nascimentolwtn@gmail.com>
2016-07-24 05:57:18 -0300

    Preparação para Java8 e JUnit5

---
+++ src/org/ita/neutrino/tparsers/junit5parser/BlockParser.java
@@ -0,0 +1,8 @@
+package org.ita.neutrino.tparsers.junit5parser;
+
+class BlockParser extends org.ita.neutrino.tparsers.junitgenericparser.BlockParser {
+
+	public void setBattery(JUnitTestBattery battery) {
+		super.setBattery(battery);
+	}
+}
