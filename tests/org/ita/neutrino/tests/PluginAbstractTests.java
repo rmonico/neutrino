39fc186e7e7ef43368bb21fa3007cc3de92ea7f8
Luiz Wagner <nascimentolwtn@gmail.com>
2016-07-30 16:33:27 -0300

    Alteração da forma como buscar arquivo neutrino.properties.

--- tests/org/ita/neutrino/tests/PluginAbstractTests.java
+++ tests/org/ita/neutrino/tests/PluginAbstractTests.java
@@ -1,9 +1,11 @@
 package org.ita.neutrino.tests;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -12,6 +14,7 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.ICompilationUnit;
@@ -73,10 +76,15 @@ public class PluginAbstractTests {
 
 		javaProject = JavaCore.create(project);
 
-		String jUnitJarPath = getJUnitJarPath();
+		String jUnit4JarPath = getJUnitJarPath("junit4.path");
+		String jUnit5JarPath = getJUnitJarPath("junit5.path");
 
 		// Cria as entradas no classpath....
-		IClasspathEntry[] cpentry = new IClasspathEntry[] { JavaCore.newSourceEntry(javaProject.getPath()), JavaRuntime.getDefaultJREContainerEntry(), JavaCore.newLibraryEntry(new Path(jUnitJarPath), null, null) };
+		IClasspathEntry[] cpentry = new IClasspathEntry[] { 
+				JavaCore.newSourceEntry(javaProject.getPath()),
+				JavaRuntime.getDefaultJREContainerEntry(), 
+				JavaCore.newLibraryEntry(new Path(jUnit4JarPath), null, null),
+				JavaCore.newLibraryEntry(new Path(jUnit5JarPath), null, null) };
 		javaProject.setRawClasspath(cpentry, javaProject.getPath(), null);
 		Map options = new HashMap();
 		options.put(DefaultCodeFormatterConstants.FORMATTER_TAB_CHAR, JavaCore.SPACE);
@@ -84,33 +92,44 @@ public class PluginAbstractTests {
 		javaProject.setOptions(options);
 
 		// Apaga o projeto de testes após rodar cada teste.
-		setDeleteTestProject(true);
+		setDeleteTestProject(false);
 	}
 
-	private String getJUnitJarPath() throws CoreException {
 
-		Properties props = new Properties();
-		
+	private String getJUnitJarPath(String key) throws CoreException {
+
 		InputStream fis = null;
-		
-		try {
-			fis = new FileInputStream(ResourcesPlugin.getWorkspace().getRoot().getLocation().toPortableString() + "/neutrino_test.properties");
-		} catch (FileNotFoundException e1) {
-			throw new RuntimeException("Arquivo de configuração de testes não encontrado...", e1);
-		}
-		
-		// try retrieve data from file
-		try {
 
-			props.load(fis);
+		try {
 			
-		} catch (IOException e) {
-			throw new RuntimeException("Erro lendo arquivo de configuração de testes...", e);
+			Properties props = new Properties();
+			
+			try {
+				URL resource = PluginAbstractTests.class.getClassLoader().getResource("neutrino_test.properties");
+				fis = new FileInputStream(new File(FileLocator.resolve(resource).toURI()));
+			} catch (URISyntaxException | IOException e1) {
+				throw new RuntimeException("Arquivo de configuração de testes não encontrado...", e1);
+			}
+			
+			// try retrieve data from file
+			try {
+	
+				props.load(fis);
+				
+			} catch (IOException e) {
+				throw new RuntimeException("Erro lendo arquivo de configuração de testes...", e);
+			}
+			
+			String junitPath = props.getProperty(key);
+			
+			return junitPath;
+		} finally {
+			if(fis != null)
+				try {
+					fis.close();
+				} catch (IOException e) {}
 		}
-
-		String junitPath = props.getProperty("junit.path");
-
-		return junitPath;
+		
 	}
 
 	@After
